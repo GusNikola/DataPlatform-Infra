7a50e189ff3bc50b5f7b0583893867c3b06040f1
Nikola Gusic <nikolagusic1028@gmail.com>
2026-03-28 10:50:32 +0100

    add bootstrap code, create tf state bucket

---
+++ terraform/bootstrap/outputs.tf
@@ -0,0 +1,4 @@
+output "state_bucket_name" {
+  description = "Name of the S3 bucket for Terraform state. Use this in environments/*/backend.tf"
+  value       = aws_s3_bucket.terraform_state.bucket
+}
