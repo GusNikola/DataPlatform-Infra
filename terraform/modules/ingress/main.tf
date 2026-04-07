036c2c9558aa5c89931d1334e09a99ca741dcc9b
Nikola Gusic <nikolagusic1028@gmail.com>
2026-04-07 06:38:35 +0200

    add general NodePool (no taint), slim ArgoCD, fix cert-manager placement

--- terraform/modules/ingress/main.tf
+++ terraform/modules/ingress/main.tf
@@ -25,14 +25,7 @@ resource "helm_release" "cert_manager" {
   atomic           = true
   cleanup_on_fail  = true
 
-  set = [
-    { name = "crds.enabled", value = "true" },
-    { name = "global.nodeSelector.role", value = "core" },
-    { name = "tolerations[0].key", value = "role" },
-    { name = "tolerations[0].value", value = "core" },
-    { name = "tolerations[0].effect", value = "NoSchedule" },
-    { name = "tolerations[0].operator", value = "Equal" },
-  ]
+  set = [{ name = "crds.enabled", value = "true" }]
 
   depends_on = [helm_release.ingress_nginx]
 }
