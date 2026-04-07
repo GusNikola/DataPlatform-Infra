5324e61bbe89943dd9c870bf90d9ed940c03c104
Nikola Gusic <nikolagusic1028@gmail.com>
2026-04-07 05:49:11 +0200

    add core/platform tolerations to ingress webhook, coredns, ebs-csi addons

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -119,7 +119,15 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.this.name
   addon_name   = "coredns"
-  depends_on   = [aws_eks_node_group.this]
+
+  configuration_values = jsonencode({
+    tolerations = [
+      { key = "role", value = "core",     effect = "NoSchedule", operator = "Equal" },
+      { key = "role", value = "platform", effect = "NoSchedule", operator = "Equal" },
+    ]
+  })
+
+  depends_on = [aws_eks_node_group.this]
 }
 
 resource "aws_eks_addon" "kube_proxy" {
@@ -174,6 +182,15 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name             = aws_eks_cluster.this.name
   addon_name               = "aws-ebs-csi-driver"
   service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+
+  configuration_values = jsonencode({
+    controller = {
+      tolerations = [
+        { key = "role", value = "core",     effect = "NoSchedule", operator = "Equal" },
+        { key = "role", value = "platform", effect = "NoSchedule", operator = "Equal" },
+      ]
+    }
+  })
 
   depends_on = [
     aws_iam_role_policy_attachment.ebs_csi_driver,
